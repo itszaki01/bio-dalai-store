a75c815d62bd9260e1e23614e97e7e8a21daece7
Blx <itszacktouil@gmail.com>
2023-12-10 14:10:07 +0100

    updated

--- src/app/romoch/page.tsx
+++ src/app/romoch/page.tsx
@@ -46,26 +46,6 @@ export default function Romoch() {
      },[])
     return (
         <Container size={isTabletAndMobile ? "xl" : "md"} p={0} pb={60}>
-            {/* <Head>
-            <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              !function(f,b,e,v,n,t,s)
-              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-              n.queue=[];t=b.createElement(e);t.async=!0;
-              t.src=v;s=b.getElementsByTagName(e)[0];
-              s.parentNode.insertBefore(t,s)}(window, document,'script',
-              'https://connect.facebook.net/en_US/fbevents.js');
-              fbq('init', '${356136510430490}');
-              fbq('track', 'PageView');
-              fbq('track', 'Purchase');
-            `,
-          }}
-        />
-        {/* End Facebook Pixel script */}
-            {/* </Head>  */}
             
             <Stack>
                 <Image src={ImgOne} style={{ width: "100%", height: "100%" }} alt="image1" />
