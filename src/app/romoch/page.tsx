611c6c00bcd855b5aba1f8fe2bec005e9bdb66f1
Blx <itszacktouil@gmail.com>
2023-12-10 13:45:22 +0100

    update

--- src/app/romoch/page.tsx
+++ src/app/romoch/page.tsx
@@ -62,6 +62,7 @@ export default function Romoch() {
               'https://connect.facebook.net/en_US/fbevents.js');
               fbq('init', '${356136510430490}');
               fbq('track', 'PageView');
+              fbq('track', 'Purchase');
             `,
           }}
         />
